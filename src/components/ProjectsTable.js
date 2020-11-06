da5debdeae671bbf568395c7fa3118617694fa78
Kevin Duck <kevingduck>
2020-11-05 21:16:55 -0500

    add datepicker

--- src/components/ProjectsTable.js
+++ src/components/ProjectsTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import Table from 'react-bootstrap/Table';
+import Table from 'react-bootstrap/Table';
 
 export class ProjectsTable extends Component {
     constructor(props) {
@@ -30,10 +30,26 @@ export class ProjectsTable extends Component {
         else {
             return (
                 <div>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Customer</th>
+                                <th>Project</th>
+                                <th>Created</th>
+                                <th>Due</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </Table>
                     <ul>
                         { items.map(item => (
                             <li key={item.id}>
-                               Customer: {item.data.customer_name} | Project {item.data.project_code} | Created On {item.data.creation_date} | Due By {item.data.due_date} | Email: {item.data.email}
+                               Customer: {item.data.customer_name} <br /> Project {item.data.project_code} | Created On {item.data.creation_date} | Due By {item.data.due_date} | Email: {item.data.email}
                             </li>
                             )
                         )}
