994f579d69bde7060403f3c6c20c004ac3237b51
Kevin Duck <kevingduck@gmail.com>
2020-11-05 22:55:11 -0500

    Update edit button

--- src/components/ProjectsTable.js
+++ src/components/ProjectsTable.js
@@ -49,7 +49,7 @@ export class ProjectsTable extends Component {
                     <ul>
                         { items.map(item => (
                             <li key={item.id}>
-                               Customer: {item.data.customer_name} <br /> Project {item.data.project_code} | Created On {item.data.creation_date} | Due By {item.data.due_date} | Email: {item.data.email} | ID: <a href={'./netlify/functions/project/'+item.id}>Edit</a>{item.id} 
+                               Customer: {item.data.customer_name} <br /> Project {item.data.project_code} | Created On {item.data.creation_date} | Due By {item.data.due_date} | Email: {item.data.email} | ID: <a href={'./netlify/functions/project/'+item.ref}>Edit</a>{item.ref} 
                             </li>
                             )
                         )}
