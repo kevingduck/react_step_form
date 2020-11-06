fc661a6077389b9c0c64bcdce46923c0c69e61af
Kevin Duck <kevingduck>
2020-11-05 20:48:28 -0500

    add close/ open functionality hack

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { UserForm } from './components/UserForm';
 import { ProjectsTable } from './components/ProjectsTable'
+import Button from '@material-ui/core/Button';
+
 
 const App = () => {
+  function refreshPage(e) {
+    e.preventDefault();
+    window.location.reload(false);
+  };
+
   return (
     <div className="App">
-      <h1>Converged Networks Project Tracker</h1>
+      <h1>Converged Networks Project Tracker <b />
+        <Button 
+          color="primary"
+          variant="contained"
+          onClick={refreshPage}>New</Button> 
+      </h1>
       <ProjectsTable></ProjectsTable>
       <UserForm />
     </div>
