1dd3312f49f6028deda571862dda2e7e02b519a9
Kevin Duck <kevingduck>
2020-11-07 12:26:18 -0500

    update table data

--- src/components/ProjectsTable.js
+++ src/components/ProjectsTable.js
@@ -10,7 +10,7 @@ export class ProjectsTable extends Component {
         }
     }
 
-    componentWillMount() {
+    componentDidMount() {
         fetch('https://converged.netlify.app/.netlify/functions/project')
         .then(res => res.json())
         .then(json => {
@@ -20,21 +20,30 @@ export class ProjectsTable extends Component {
             })
         });
     }
-
-    componentDidMount() {
-        console.log('test');
-    }
     
+    delete = e => {
+        e.preventDefault();
+        debugger;
+        const value  = e.target.value;
+        if (window.confirm("Delete Project?" + value)) {
+          fetch('./.netlify/functions/project/delete/', {
+            method: 'DELETE',
+            body: JSON.stringify(value),
+          });
+          console.log('deleted' + value);
+        };
+      
+      };
     //TODO: Add delete and edit function
 
     render() {
         var { isLoaded, items } = this.state;
 
-        items = [{"ref":{"@ref":{"id":"281142982455853568","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377691716000,"data":{"customer_name":"Charleston County Schools","shipping_address":"1010 Calhoun St","email":"demo-email@sc.edu","due_date":"10/02/2023","creation_date":"Mon Nov 02 2020","project_code":"CHAR-test123"}},{"ref":{"@ref":{"id":"281143013386748418","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377721150000,"data":{"customer_name":"Greenville County Schools","shipping_address":"123 Demo Ave","email":"another_demo-email@gmail.com","due_date":"02/02/2024","creation_date":"Mon Nov 02 2020","project_code":"GREE-test123"}},{"ref":{"@ref":{"id":"281143108011295234","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377811400000,"data":{"customer_name":"Duck County Schools","shipping_address":"2847 demo street ","email":"Testemail@enail.com","due_date":"07/03/2021","creation_date":"Mon Nov 02 2020","project_code":"DUCK-test123"}},{"ref":{"@ref":{"id":"281144590129431040","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604379224880000,"data":{"customer_name":"Asheville County Schools","shipping_address":"123 test avenue","email":"Demo@demo.com","due_date":"10/20/2024","creation_date":"Mon Nov 02 2020","project_code":"ASHE-test123"}},{"ref":{"@ref":{"id":"281405222268436994","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604627783045000,"data":{"customer_name":"Maricopa County","shipping_address":"1600 Penn Ave","email":"aasdf@asdf.com","due_date":"2020-12-31","creation_date":"Thu Nov 05 2020","project_code":"MARI-test123"}},{"ref":{"@ref":{"id":"281407659078320640","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604630106910000,"data":{"customer_name":"Test","shipping_address":"","email":"","creation_date":"Thu Nov 05 2020","project_code":"TEST-test123"}}];
+        // items = [{"ref":{"@ref":{"id":"281142982455853568","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377691716000,"data":{"customer_name":"Charleston County Schools","shipping_address":"1010 Calhoun St","email":"demo-email@sc.edu","due_date":"10/02/2023","creation_date":"Mon Nov 02 2020","project_code":"CHAR-test123"}},{"ref":{"@ref":{"id":"281143013386748418","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377721150000,"data":{"customer_name":"Greenville County Schools","shipping_address":"123 Demo Ave","email":"another_demo-email@gmail.com","due_date":"02/02/2024","creation_date":"Mon Nov 02 2020","project_code":"GREE-test123"}},{"ref":{"@ref":{"id":"281143108011295234","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377811400000,"data":{"customer_name":"Duck County Schools","shipping_address":"2847 demo street ","email":"Testemail@enail.com","due_date":"07/03/2021","creation_date":"Mon Nov 02 2020","project_code":"DUCK-test123"}},{"ref":{"@ref":{"id":"281144590129431040","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604379224880000,"data":{"customer_name":"Asheville County Schools","shipping_address":"123 test avenue","email":"Demo@demo.com","due_date":"10/20/2024","creation_date":"Mon Nov 02 2020","project_code":"ASHE-test123"}},{"ref":{"@ref":{"id":"281405222268436994","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604627783045000,"data":{"customer_name":"Maricopa County","shipping_address":"1600 Penn Ave","email":"aasdf@asdf.com","due_date":"2020-12-31","creation_date":"Thu Nov 05 2020","project_code":"MARI-test123"}},{"ref":{"@ref":{"id":"281407659078320640","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604630106910000,"data":{"customer_name":"Test","shipping_address":"","email":"","creation_date":"Thu Nov 05 2020","project_code":"TEST-test123"}}];
         
         var url = "./.netlify/functions/project/";
 
-        if (!isLoaded) {
+        if (isLoaded) {
             return <div>Loading ... </div>
         } 
         else {
@@ -61,7 +70,7 @@ export class ProjectsTable extends Component {
                                     <td>{item.data.due_date}</td>
                                     <td>{item.data.email}</td>
                                     <td><a href={url + item.ref["@ref"].id}>Edit</a></td>
-                                    <td><a href={url + item.ref["@ref"].id}>Delete</a></td>
+                                    <td><button value={item.ref["@ref"].id} onClick={this.delete}>Delete</button></td>
                                 </tr>
                                 )
                             )}
