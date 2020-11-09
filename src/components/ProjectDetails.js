78a0624875e3679b3eb4d8365589ffaaa68c2246
Kevin Duck <kevingduck>
2020-11-09 14:26:03 -0500

    start adding items form

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react'
 
+
+// Get data from Fauna
+// Pre-populate data in form
+// SUbmit changes to Fauna
+
 export class ProjectDetails extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+        }
+    }
+    
     render() {
-        return (
-            <div>
-                <span>ProjectDetails</span>
-            </div>
-        )
+
+        if (!isLoaded) {
+            return <div>Loading ... </div>
+        }
+        else {
+            return (
+                <div style={{ padding: '20px' }}>
+                    <Table responsive striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Customer</th>
+                                <th>Dates</th>
+                                <th>Contact</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {items.map(item => (
+                                <tr key={item.id}>
+                                    <td><a href={url + item.ref["@ref"].id}>{item.data.customer_name} - {item.data.project_code}</a></td>
+                                    <td>Created {item.data.creation_date} <br /> Due {item.data.due_date}</td>
+                                    <td><a href={"mailto:" + item.data.email}>{item.data.email}</a></td>
+                                </tr>
+                            )
+                            )}
+                        </tbody>
+                    </Table>
+                </div>
+            )
+        }
     }
 }
 
--- src/components/ProjectsTable.js
+++ src/components/ProjectsTable.js
@@ -7,66 +7,94 @@ export class ProjectsTable extends Component {
         this.state = {
             items: [],
             isLoaded: false,
+            itemToUpdate: [],
         }
     }
 
     componentDidMount() {
         fetch('https://converged.netlify.app/.netlify/functions/project')
-        .then(res => res.json())
-        .then(json => {
-            this.setState({
-                isLoaded: true,
-                items: json,
-            })
-        });
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
     }
-    
+
     delete = e => {
         e.preventDefault();
-        const value  = e.target.value;
-        debugger;
+        const value = e.target.value;
         if (window.confirm("Delete Project? " + value)) {
-          fetch('./.netlify/functions/project/' + value, {
-            method: 'DELETE',
-          });
-          console.log('Deleted project' + value);
+            fetch('./.netlify/functions/project/' + value, {
+                method: 'DELETE',
+            });
+            console.log('Deleted project' + value);
         };
-      };
+    };
+
+    // edit = e => {
+    //     // e.preventDefault();
+    //     // const value = e.target.value;
+    //     // console.log(value);
+    //     debugger;
+    // }
+
+    edit = e => {
+        e.preventDefault();
+        const value = e.target.value;
+        console.log(value);
+        fetch('./.netlify/functions/project/' + value, {
+            method: 'GET',
+        })
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    itemToUpdate: json,
+                })
+            });
+        debugger;
+    };
 
     //TODO: Add edit function
 
     render() {
         var { isLoaded, items } = this.state;
 
-        // items = [{"ref":{"@ref":{"id":"281142982455853568","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377691716000,"data":{"customer_name":"Charleston County Schools","shipping_address":"1010 Calhoun St","email":"demo-email@sc.edu","due_date":"10/02/2023","creation_date":"Mon Nov 02 2020","project_code":"CHAR-test123"}},{"ref":{"@ref":{"id":"281143013386748418","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377721150000,"data":{"customer_name":"Greenville County Schools","shipping_address":"123 Demo Ave","email":"another_demo-email@gmail.com","due_date":"02/02/2024","creation_date":"Mon Nov 02 2020","project_code":"GREE-test123"}},{"ref":{"@ref":{"id":"281143108011295234","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377811400000,"data":{"customer_name":"Duck County Schools","shipping_address":"2847 demo street ","email":"Testemail@enail.com","due_date":"07/03/2021","creation_date":"Mon Nov 02 2020","project_code":"DUCK-test123"}},{"ref":{"@ref":{"id":"281144590129431040","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604379224880000,"data":{"customer_name":"Asheville County Schools","shipping_address":"123 test avenue","email":"Demo@demo.com","due_date":"10/20/2024","creation_date":"Mon Nov 02 2020","project_code":"ASHE-test123"}},{"ref":{"@ref":{"id":"281405222268436994","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604627783045000,"data":{"customer_name":"Maricopa County","shipping_address":"1600 Penn Ave","email":"aasdf@asdf.com","due_date":"2020-12-31","creation_date":"Thu Nov 05 2020","project_code":"MARI-test123"}},{"ref":{"@ref":{"id":"281407659078320640","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604630106910000,"data":{"customer_name":"Test","shipping_address":"","email":"","creation_date":"Thu Nov 05 2020","project_code":"TEST-test123"}}];
-        
+        items = [{"ref":{"@ref":{"id":"281142982455853568","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377691716000,"data":{"customer_name":"Charleston County Schools","shipping_address":"1010 Calhoun St","email":"demo-email@sc.edu","due_date":"10/02/2023","creation_date":"Mon Nov 02 2020","project_code":"CHAR-test123"}},{"ref":{"@ref":{"id":"281143013386748418","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377721150000,"data":{"customer_name":"Greenville County Schools","shipping_address":"123 Demo Ave","email":"another_demo-email@gmail.com","due_date":"02/02/2024","creation_date":"Mon Nov 02 2020","project_code":"GREE-test123"}},{"ref":{"@ref":{"id":"281143108011295234","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604377811400000,"data":{"customer_name":"Duck County Schools","shipping_address":"2847 demo street ","email":"Testemail@enail.com","due_date":"07/03/2021","creation_date":"Mon Nov 02 2020","project_code":"DUCK-test123"}},{"ref":{"@ref":{"id":"281144590129431040","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604379224880000,"data":{"customer_name":"Asheville County Schools","shipping_address":"123 test avenue","email":"Demo@demo.com","due_date":"10/20/2024","creation_date":"Mon Nov 02 2020","project_code":"ASHE-test123"}},{"ref":{"@ref":{"id":"281405222268436994","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604627783045000,"data":{"customer_name":"Maricopa County","shipping_address":"1600 Penn Ave","email":"aasdf@asdf.com","due_date":"2020-12-31","creation_date":"Thu Nov 05 2020","project_code":"MARI-test123"}},{"ref":{"@ref":{"id":"281407659078320640","collection":{"@ref":{"id":"projects","collection":{"@ref":{"id":"collections"}}}}}},"ts":1604630106910000,"data":{"customer_name":"Test","shipping_address":"","email":"","creation_date":"Thu Nov 05 2020","project_code":"TEST-test123"}}];
+
         var url = "./.netlify/functions/project/";
 
-        if (!isLoaded) {
+        if (isLoaded) {
             return <div>Loading ... </div>
-        } 
+        }
         else {
             return (
-                <div style={{padding:'20px'}}>
-                    <Table responsive striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>Customer</th>
-                                <th>Dates</th>
-                                <th>Email</th>
-                            </tr>
-                        </thead>
-                        <tbody> 
-                            { items.map(item => (
-                                <tr key={item.id}>
-                                    <td><a href={url + item.ref["@ref"].id}>{item.data.customer_name} - {item.data.project_code}</a></td>
-                                    <td>Created {item.data.creation_date} <br/> Due {item.data.due_date}</td>
-                                    <td><a href={"mailto:" + item.data.email}>{item.data.email}</a></td>
+                <div>
+                    <div style={{ padding: '20px' }}>
+                        <Table responsive striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>Customer</th>
+                                    <th>Dates</th>
+                                    <th>Contact</th>
                                 </tr>
+                            </thead>
+                            <tbody>
+                                {items.map(item => (
+                                    <tr key={item.ref["@ref"].id}>
+                                        <td><a href={url + item.ref["@ref"].id}>{item.data.customer_name} - {item.data.project_code}</a><button onClick={() => this.edit}>Edit</button></td>
+                                        <td>Created {item.data.creation_date} <br /> Due {item.data.due_date}</td>
+                                        <td><a href={"mailto:" + item.data.email}>{item.data.email}</a></td>
+                                    </tr>
                                 )
-                            )}
-                        </tbody>
-                    </Table>
+                                )}
+                            </tbody>
+                        </Table>
+                    </div>
+                    <div>
+                        <h1>Details</h1>
+                    </div>
                 </div>
             )
         }
